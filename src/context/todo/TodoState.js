3af30c227fc5b8e33836a7673ada2ac030c3a837
Tender6796 <tender6795@gmail.com>
2019-12-27 11:57:31 +0200

    add to firebase error log

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -2,17 +2,39 @@ import React, { useReducer, useContext } from 'react';
 import { Alert } from 'react-native'
 import { TodoContext } from './todoContext';
 import { todoReducer } from './todoReducer'
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types';
 import { ScreenContext } from '../screen/screenContext';
+import {
+    ADD_TODO,
+    REMOVE_TODO,
+    UPDATE_TODO,
+    SHOW_LOADER,
+    HIDE_LOADER,
+    SHOW_ERROR,
+    CLEAR_ERROR,
+    FETCH_TODOS
+} from '../types';
+
 
 export const TodoState = ({ children }) => {
     const { changeScreen } = useContext(ScreenContext)
     const initialState = {
-        todos: [{ id: '1', title: 'Выучить React Native' },]
+        todos: [],
+        loading: false,
+        error: null
     }
     const [state, dispatch] = useReducer(todoReducer, initialState)
 
-    const addTodo = title => dispatch({ type: ADD_TODO, title })
+    const addTodo = async title => {
+        const responce = await fetch('https://rn-todo-app-c79d8.firebaseio.com/todos.json', {
+            method: "POST",
+            headers: { 'Content-Type': 'aplication/json' },
+            body: JSON.stringify({ title })
+        })
+        const data = await responce.json()
+        dispatch({ type: ADD_TODO, title, id: data.name })
+
+    }
+
     const removeTodo = id => {
         const todo = state.todos.find(t => t.id === id)
         Alert.alert(
@@ -37,14 +59,48 @@ export const TodoState = ({ children }) => {
 
 
     }
+
+    const fetchTodos = async () => {
+        showLoader()
+        clearError()
+        try {
+            const responce = await fetch('', {
+                method: "GET",
+                headers: { 'Content-Type': 'aplication/json' },
+            })
+            const data = await responce.json()
+            console.log('Fetch data ', data)
+            const todos = Object.keys(data).map(key => ({ ...data[key], id: key }))
+            dispatch({ type: FETCH_TODOS, todos })
+        } catch (error) {
+            showError('Что то пошло не так....')
+            console.log(error)
+        }finally{
+            hideLoader()
+        }
+    }
+
     const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
+
+    const showLoader = () => dispatch({ type: SHOW_LOADER })
+
+    const hideLoader = () => dispatch({ type: HIDE_LOADER })
+
+    const showError = error => dispatch({ type: SHOW_ERROR, error })
+
+    const clearError = () => dispatch({ type: CLEAR_ERROR })
+
+
     return (
         <TodoContext.Provider
             value={{
                 todos: state.todos,
+                loading: state.loading,
+                error: state.error,
                 addTodo,
                 removeTodo,
-                updateTodo
+                updateTodo,
+                fetchTodos
             }}
         >
             {children}
