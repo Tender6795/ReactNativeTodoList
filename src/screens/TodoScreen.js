27a688b43804cea2e957e7f205eb4c63fe869fea
Tender6796 <tender6795@gmail.com>
2019-12-27 11:57:01 +0200

    Adaptive orientation MainScreen

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import { FontAwesome, AntDesign } from '@expo/vector-icons'
 import { THEME } from '../theme';
 import { AppCard } from '../components/ui/AppCard';
@@ -60,7 +60,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     button: {
-        width: '40%'
+        width: Dimensions.get('window').width / 3
     },
     title: {
         fontSize: 20
